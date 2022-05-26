bb1cd3d3bca33e86a05ef4eb6849a7553f9f06d5
seffu <seffukioi@gmail.com>
2022-05-26 07:04:09 +0300

    others.sql updated

--- Others.sql
+++ Others.sql
@@ -7,3 +7,124 @@ SELECT
             LAG(status,1,status) OVER (ORDER BY ASC) AS prev_status
         FROM event_status 
     ) A
+    
+-- CACULATE THE CANCELLATION RATE FOR CLIENTS AND DRIVERS WHO WERE NOT BANNED BASED ON DAYS. RIDES CAN BE CANCELED BY EITHER CLIENT OR DRIVER
+
+Create table  Trips (id int, client_id int, driver_id int, city_id int, status varchar(50), request_at varchar(50));
+Create table Users (users_id int, banned varchar(50), role varchar(50));
+Truncate table Trips;
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('1', '1', '10', '1', 'completed', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('2', '2', '11', '1', 'cancelled_by_driver', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('3', '3', '12', '6', 'completed', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('4', '4', '13', '6', 'cancelled_by_client', '2013-10-01');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('5', '1', '10', '1', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('6', '2', '11', '6', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('7', '3', '12', '6', 'completed', '2013-10-02');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('8', '2', '12', '12', 'completed', '2013-10-03');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('9', '3', '10', '12', 'completed', '2013-10-03');
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('10', '4', '13', '12', 'cancelled_by_driver', '2013-10-03');
+Truncate table Users;
+insert into Users (users_id, banned, role) values ('1', 'No', 'client');
+insert into Users (users_id, banned, role) values ('2', 'Yes', 'client');
+insert into Users (users_id, banned, role) values ('3', 'No', 'client');
+insert into Users (users_id, banned, role) values ('4', 'No', 'client');
+insert into Users (users_id, banned, role) values ('10', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('11', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('12', 'No', 'driver');
+insert into Users (users_id, banned, role) values ('13', 'No', 'driver');
+
+
+WITH cte AS(
+SELECT
+request_at, 
+1.0 * (COUNT(CASE WHEN status IN ('cancelled_by_client','cancelled_by_driver') THEN 1 ELSE NULL END)) AS canceled_trips,
+1.0*(COUNT (0)) AS total_trips
+FROM trips t
+INNER JOIN  users u 
+ON t.client_id = u.users_id
+where u.banned = 'No'
+GROUP BY request_at
+)
+
+select request_at,canceled_trips,total_trips,ROUND(((canceled_trips/total_trips)*100),2) AS cancellation_rate
+FROM cte
+
+-- GET PLAYERS WITH THE HIGHEST SCORES BASED ON THEIR GROUPS AND IF THERE IS A TIE, THE ONE WITH THE SMALLEST GROUP ID COMES FIRST
+
+create table players
+(player_id int,
+group_id int)
+
+insert into players values (15,1);
+insert into players values (25,1);
+insert into players values (30,1);
+insert into players values (45,1);
+insert into players values (10,2);
+insert into players values (35,2);
+insert into players values (50,2);
+insert into players values (20,3);
+insert into players values (40,3);
+
+create table matches
+(
+match_id int,
+first_player int,
+second_player int,
+first_score int,
+second_score int)
+
+insert into matches values (1,15,45,3,0);
+insert into matches values (2,30,25,1,2);
+insert into matches values (3,30,15,2,0);
+insert into matches values (4,40,20,5,2);
+insert into matches values (5,35,50,1,1);
+
+WITH player_scores AS
+(
+    SELECT first_player as player_id, first_score as score
+    FROM matches
+    UNION ALL
+    SELECT second_player as player_id, second_score as score
+    FROM matches
+),final_scores AS
+(
+    SELECT ps.player_id,SUM(score) score,p.group_id 
+    FROM player_scores ps
+    INNER JOIN players p
+    ON ps.player_id = p.player_id
+    GROUP BY ps.player_id,p.group_id
+)
+
+SELECT 
+    *, 
+    RANK() OVER(PARTITION BY group_id ORDER BY score DESC, player_id ASC) AS rn
+FROM final_scores;
+
+
+
+create table users (
+user_id         int     ,
+ join_date       date    ,
+ favorite_brand  varchar(50));
+
+ create table orders (
+ order_id       int     ,
+ order_date     date    ,
+ item_id        int     ,
+ buyer_id       int     ,
+ seller_id      int 
+ );
+
+ create table items
+ (
+ item_id        int     ,
+ item_brand     varchar(50)
+ );
+
+
+ insert into users values (1,'2019-01-01','Lenovo'),(2,'2019-02-09','Samsung'),(3,'2019-01-19','LG'),(4,'2019-05-21','HP');
+
+ insert into items values (1,'Samsung'),(2,'Lenovo'),(3,'LG'),(4,'HP');
+
+ insert into orders values (1,'2019-08-01',4,1,2),(2,'2019-08-02',2,1,3),(3,'2019-08-03',3,2,3),(4,'2019-08-04',1,4,2)
+ ,(5,'2019-08-04',1,3,4),(6,'2019-08-05',2,2,4);
